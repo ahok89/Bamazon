aef36afd06c9abec2a25993d080516c70526468c
Andrew Hok <ahok89@gmail.com>
2019-07-03 22:43:37 -0400

    added items to the database

--- bamzonSchema.sql
+++ bamzonSchema.sql
@@ -11,3 +11,6 @@ CREATE TABLE products(
   stock_quantity INT default 0,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
+VALUES ("Toshiba Laptop", "Electronics", 650.50, 100), ("Playstation 4", "Electronics" 300.00, 75), ("Dog Food", "Pets", 40.00, 55), ("Cat Food", "Pets", 35.00, 55), ("Exercise Ball", "Fitness", 15.00, 50), ("Dumbbell", "Fitness", 7.00, 50),("Notebook paper", "Stationary Supplies", 2.00, 150), ("Ballpoint Pen", "Stationary Supplies", 1.75, 150), ("Scooter", "Recreation", 99.00, 60), ("Skateboard", "Recreation", 70.00, 60), ("Pizza", "Food", 6.00, 314), ("Cherry Pie", "Food", 3.75, 314) ;

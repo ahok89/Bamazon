66756ea8546885375e07baf3449b088016b97be1
Andrew Hok <ahok89@gmail.com>
2019-07-03 22:22:31 -0400

    deleted revision to node.js

--- bamzonCustomer.js
+++ bamzonCustomer.js
@@ -12,28 +12,3 @@ var connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    start();
-  });
-
-  function start() {
-    inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "list",
-        message: "Would you like to [POST] an auction or [BID] on an auction?",
-        choices: ["POST", "BID", "EXIT"]
-      })
-      .then(function(answer) {
-    
-        if (answer.postOrBid === "POST") {
-          postAuction();
-        }
-        else if(answer.postOrBid === "BID") {
-          bidAuction();
-        } else{
-          connection.end();
-        }
-      });
-  }
